de4f6f4f8f4c239e56fe085186cc64bb7621618c
Nickleous <nickleous@gmail.com>
2020-01-06 19:03:28 -0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplayMessage = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDisplayMessage
+        '
+        Me.btnDisplayMessage.Location = New System.Drawing.Point(91, 151)
+        Me.btnDisplayMessage.Name = "btnDisplayMessage"
+        Me.btnDisplayMessage.Size = New System.Drawing.Size(75, 60)
+        Me.btnDisplayMessage.TabIndex = 0
+        Me.btnDisplayMessage.Text = "Display Message"
+        Me.btnDisplayMessage.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(88, 88)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(82, 13)
+        Me.lblMessage.TabIndex = 1
+        Me.lblMessage.Text = "Click the Button"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnDisplayMessage)
+        Me.Name = "Form1"
+        Me.Text = "My First Project"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplayMessage As Windows.Forms.Button
+    Friend WithEvents lblMessage As Windows.Forms.Label
+End Class
